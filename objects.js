4d6c76f47920b68c77fa18e6466f3a70a673c0d8
Simon Teo <teosimon78@gmail.com>
2020-03-25 20:10:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {'duckman' : 'Bobbamir'};
 
 function updatePlaylist(playlist, artistName, songTitle)
 {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist;
 }
 
